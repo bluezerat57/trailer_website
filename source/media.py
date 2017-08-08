21a5748cb388731e050cd88c2e0a8ee481f6e893
bluezerat57 <jlreyes57@gmail.com>
2017-08-08 20:32:25 +0900

    Changed media.py to meet PEP8 style guide

--- source/media.py
+++ source/media.py
@@ -1,11 +1,15 @@
 import webbrowser
 
+
 class Movie():
-    """Movie object class. Has a constructor and a function to show the movie's trailer"""
-    
+    """Movie object class.
+       Has a constructor and a function to show the movie's trailer"""
     # Constructor class for Movie
-    def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
-        """Movie class constructor.\nMust be initialized with a title, storyline, poster image (url) and a link to its trailer (url)"""
+    def __init__(self, title, storyline,
+                 poster_image_url, trailer_youtube_url):
+        """Movie class constructor.\n
+           Must be initialized with a title, storyline, poster image (url)
+           and a link to its trailer (url)"""
         self.title = title
         self.storyline = storyline
         self.poster_image_url = poster_image_url
